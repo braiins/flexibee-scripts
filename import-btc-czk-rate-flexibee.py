36172f588e56c4d4277b55324fb3986dd2da6f53
Jan Čapek <jan.capek@braiins.cz>
2015-01-02 16:38:35 +0100

    import-btc-czk-rate-flexibee - refactoring due to flexibee API consolidation
    - the API is now able to send the request to flexibee, the script has
      been simplified accordingly

--- import-btc-czk-rate-flexibee.py
+++ import-btc-czk-rate-flexibee.py
@@ -15,19 +15,17 @@ if len(sys.argv) != 3:
     print "Synopsis: %s user password" % os.path.basename(sys.argv[0])
     sys.exit(2)
 
-acc_url = "https://acc.bnet:5434/c/braiins_systems_s_r_o_1/kurz.json"
+acc_url = "https://acc.bnet:5434/c/braiins_systems_s_r_o_1"
 
-# fetch all exchange rates
-rates = []
+
+req = flexibee.api.RateRequest()
 for line in sys.stdin:
     (date, none1, none2, btc_czk) = line.rstrip().split(';')
-    rates.append(flexibee.api.ExchangeRate(date, btc_czk))
+    req.append(flexibee.api.ExchangeRate(date, btc_czk))
 
-rate_req = flexibee.api.RateRequest(rates).to_json()
 
-print "Sending: %s" % rate_req
+print "Sending: %s" % req
 
-response = requests.put(acc_url, data=rate_req, auth=(sys.argv[1], sys.argv[2]),
-			verify=False)
+response = req.send(acc_url, sys.argv[1], sys.argv[2])
 
 print response.content
